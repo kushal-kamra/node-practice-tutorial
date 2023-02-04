f6f56274fac7e5677a8a617e294d34e86d90ad4e
kushal-kamra <kushalkamradev@gmail.com>
2023-02-04 15:27:18 +0530

    adding load balancing using haproxy for server sent events

--- server-sent-event/index.js
+++ server-sent-event/index.js
@@ -1,4 +1,5 @@
 const app = require("express")();
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.send("hello!");
@@ -13,12 +14,12 @@ app.get("/stream", (req, res) => {
 let i = 0;
 function send(res) {
     i++;
-    res.write("data:" + `hello! ${i}\n\n`);
+    res.write("data:" + `hello from ${port}! ${i}\n\n`);
     setTimeout(() => send(res), 1000);
 }
 
-app.listen(8080);
-console.log("Listening on 8080");
+app.listen(port);
+console.log(`Listening on ${port}`);
 
 // Run below in browser console to establish connection
 // let sse = new EventSource("http://localhost:8080/stream");
